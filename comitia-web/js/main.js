ca9b97c19d0c8c64df8d05c02103acaba3347e6a
Shimabukuro Yuki <shima.nobishiro@gmail.com>
2018-03-11 20:25:56 +0900

    Add files via upload

---
+++ comitia-web/js/main.js
@@ -0,0 +1,11 @@
+
+
+window.onload = function(){
+	document.getElementById("root").classList.add("scroll-lock");
+
+	setTimeout(function (){
+    	document.getElementById("root").classList.remove("scroll-lock");
+  }, 2600)
+};
+
+
